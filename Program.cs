2cc6dc521a511be6d5c08c39d411acc27b163f36
Dmitriy <3292203@gmail.com>
2023-05-26 21:40:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+while (true)
+{
+    try
+    {
+        Console.Write("Введите количество одногруппников: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество страниц каждому: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (a < 0 || b < 0)
+        {
+            Console.WriteLine("0");
+        }
+        else
+        {
+            Console.WriteLine($"Количество страниц которое необходимо распечатать: {a * b}");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Неправильный тип данных");
+    }
+   
+}
